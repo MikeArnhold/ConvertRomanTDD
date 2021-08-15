3d58a06d218fdd80c6771eab698aa5bff9ec0ca9
Mike Arnhold <wiwg@gmx.net>
2021-08-15 10:03:11 +0200

    feat(converter): 1st implementation

--- ConvertTests.cs
+++ ConvertTests.cs
@@ -5,6 +5,71 @@ namespace RomanNumeralsTDD20210815
 	[TestClass]
 	public class ConvertTests
 	{
+		[TestMethod]
+		public void Test1()
+		{
+			Assert.AreEqual("I", this.Convert(1));
+		}
+
+		[TestMethod]
+		public void Test2()
+		{
+			Assert.AreEqual("II", this.Convert(2));
+		}
+
+		[TestMethod]
+		public void Test5()
+		{
+			Assert.AreEqual("V", this.Convert(5));
+		}
+
+		[TestMethod]
+		public void Test4()
+		{
+			Assert.AreEqual("IV", this.Convert(4));
+		}
+
+		[TestMethod]
+		public void Test10()
+		{
+			Assert.AreEqual("X", this.Convert(10));
+		}
+
+		[TestMethod]
+		public void Test9()
+		{
+			Assert.AreEqual("IX", this.Convert(9));
+		}
+
+		[TestMethod]
+		public void Test11()
+		{
+			Assert.AreEqual("XI", this.Convert(11));
+		}
+
+		[TestMethod]
+		public void Test24()
+		{
+			Assert.AreEqual("XXIV", this.Convert(24));
+		}
+
+		[TestMethod]
+		public void Test61()
+		{
+			Assert.AreEqual("LXI", this.Convert(61));
+		}
+
+		[TestMethod]
+		public void Test561()
+		{
+			Assert.AreEqual("DLXI", this.Convert(561));
+		}
+
+		[TestMethod]
+		public void Test2999()
+		{
+			Assert.AreEqual("MMCMXCIX", this.Convert(2999));
+		}
 
 		private string Convert(int number) => RomanNumeralConverter.Convert(number);
 	}
--- RomanNumeralConverter.cs
+++ RomanNumeralConverter.cs
@@ -1,9 +1,59 @@
+using System;
+
 namespace RomanNumeralsTDD20210815
 {
 	static class RomanNumeralConverter
 	{
+		private static string[][] charsets = {
+			new string[] { "C", "D", "M" },
+			new string[] { "X", "L", "C" },
+			new string[] { "I", "V", "X" },
+		};
+		private static int[][] limits = {
+			new int[] {100, 500},
+			new int[] {10, 50},
+			new int[] {1, 5},
+		};
+
+		private static string Convert(string[] chars, int[] limits, ref int number) {
+			string roman = "";
+			int before = number;
+			while (number > 0) {
+				if (number - limits[1] >= 0) {
+					roman += chars[1];
+					number -= limits[1];
+				} else if (number - limits[0] >= 0) {
+					roman += chars[0];
+					number -= limits[0];
+				} else {
+					break;
+				}
+				// IIII _> IV
+				if (roman.Substring(Math.Max(0, roman.Length - 4)) == $"{chars[0]+chars[0]+chars[0]+chars[0]}") {
+					roman = roman.Remove(roman.Length - 4) + $"{chars[0]+chars[1]}";
+				}
+				// VV -> X
+				if (roman.Substring(Math.Max(0, roman.Length - 2)) == $"{chars[1]+chars[1]}") {
+					roman = roman.Remove(roman.Length - 2) + $"{chars[2]}";
+				}
+				// VIV -> IX
+				if (roman.Substring(Math.Max(0, roman.Length - 3)) == $"{chars[1]+chars[0]+chars[1]}") {
+					roman = roman.Remove(roman.Length - 3) + $"{chars[0]+chars[2]}";
+				}
+			}
+			return roman;
+		}
+
 		public static string Convert(int number) {
-			return string.Empty;
+			string roman = "";
+			for(int i = 0; i < RomanNumeralConverter.charsets.Length; ++i) {
+				roman += RomanNumeralConverter.Convert(
+					RomanNumeralConverter.charsets[i],
+					RomanNumeralConverter.limits[i],
+					ref number
+				);
+			}
+			return roman;
 		}
 	}
 }
